00a1d97e75194ae7fe79cb67eb10f05c531722d5
dDeepak26 <deepakdurgam26@gmail.com>
2024-09-11 22:32:52 +0530

    Project Setup, Login Screen UI, Authentication

---
+++ app/index.jsx
@@ -0,0 +1,16 @@
+import { Link } from "expo-router";
+import { Pressable, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <Link href={"/login"}>
+        <Text>Go To Login Screen</Text>
+      </Link>
+    </View>
+  );
+}
